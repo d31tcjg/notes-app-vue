dd6223a97e609a346dac984c29219fe80a6e8ecb
Cesar Garcia <d31tcjg@gmail.com>
2021-04-20 14:29:49 -0500

    added notes design

---
+++ src/components/Note.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="bg-yellow-500 rounded-sm text-center">
+    <div class="flex justify-end space-x-2">
+      <button class="hover:text-green-800"><ion-edit class="mt-1" /></button>
+      <button class="hover:text-red-500">
+        <zondicons-close class="mt-1 mr-1" />
+      </button>
+    </div>
+    <h3 class="text-2xl font-thin tracking tracking-wider p-1 space-y-2">
+      Note Title
+    </h3>
+    <p class="text-sm font-medium mb-6">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quasi fuga
+      ab iste totam provident nisi accusantium officiis consequatur voluptates
+      ex quam, quisquam pariatur sed fugit ea vel, mollitia consequuntur.
+    </p>
+  </div>
+</template>
---
+++ src/components/NotesAdd.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="bg-yellow-500 rounded-sm text-center flex justify-center">
+    <button
+      class="opacity-40 text-5xl transform transition-all hover:scale-150 hover:opacity-100"
+    >
+      <ic:round-add />
+    </button>
+  </div>
+</template>
